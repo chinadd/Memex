70af91f0f84cd82e78b99d40d690585ee3361d9c
Vincent den Boer <vincent@shishkabab.net>
2018-03-02 12:17:14 +0100

    Work on import/export

--- src/search/index.test.ts
+++ src/search/index.test.ts
@@ -1,10 +1,13 @@
 /* eslint-env jest */
 
 import memdown from 'memdown'
+import encode from 'encoding-down'
 import fakeIDBFactory from 'fake-indexeddb'
 import fakeIDBKeyRange from 'fake-indexeddb/lib/FDBKeyRange'
 import * as search from './'
 import * as oldIndex from './search-index-old'
+import { exportPages as exportOldPages } from './search-index-old/export'
+import { importPage as importNewPage } from './search-index-new/import'
 import * as newIndex from './search-index-new'
 
 async function doIntegrationTest() {
@@ -125,6 +128,48 @@ describe('Old search index', () => {
         oldIndex.init({ levelDown: memdown() })
         await doIntegrationTest()
     })
+
+    test('Exporting data', async () => {
+        search.getBackend._reset({ useOld: true })
+        oldIndex.init({ levelDown: memdown() })
+
+        const visit1 = Date.now().toString()
+        await search.addPage({
+            pageDoc: {
+                _id: 'test-id-1',
+                url: 'https://www.test.com/test',
+                content: {
+                    fullText: 'the wild fox jumped over the hairy red hen',
+                    title: 'test page',
+                },
+            },
+            bookmarkDocs: [],
+            visits: [visit1],
+        })
+        const visit2 = Date.now().toString()
+        await search.addPage({
+            pageDoc: {
+                _id: 'test-id-2',
+                url: 'https://www.test.com/test2',
+                content: {
+                    fullText: 'the fox was wild',
+                    title: 'test page 2',
+                },
+            },
+            bookmarkDocs: [],
+            visits: [visit2],
+        })
+
+        const stream = exportOldPages()
+        await new Promise((resolve, reject) => {
+            stream
+                .on('data', (obj) => {
+                    console.log('Got data!', obj)
+                })
+                .on('error', reject)
+                .on('end', resolve)
+        })
+    })
 })
 
 describe('New search index', () => {
@@ -136,5 +181,29 @@ describe('New search index', () => {
             dbName: 'dexie',
         })
         await doIntegrationTest()
+    })
+
+    test('Importing data', async () => {
+        search.getBackend._reset({ useOld: false })
+        newIndex.init({
+            indexedDB: fakeIDBFactory,
+            IDBKeyRange: fakeIDBKeyRange,
+            dbName: 'dexie',
+        })
+        await importNewPage({
+            url: 'https://test.com',
+            content: {
+                title: 'very interesting futile title',
+                fullText: 'body text with some useless filling stuff',
+            },
+            visits: [],
+            tags: [],
+            bookmark: null
+        })
+        // const { docs: results } = await search.search({
+        //     query: 'fox',
+        //     mapResultsFunc: async results => results,
+        // })
+        // console.log(results)
     })
 })
