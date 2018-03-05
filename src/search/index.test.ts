a54bb76832fe446215d53270c8c0bd4965b56aa8
Vincent den Boer <vincent@shishkabab.net>
2018-03-05 19:47:10 +0100

    Progress on export

--- src/search/index.test.ts
+++ src/search/index.test.ts
@@ -4,18 +4,19 @@ import memdown from 'memdown'
 import encode from 'encoding-down'
 import * as fakeIDBFactory from 'fake-indexeddb'
 import * as fakeIDBKeyRange from 'fake-indexeddb/lib/FDBKeyRange'
+import db from '../pouchdb'
 import * as search from './'
 import * as oldIndex from './search-index-old'
 import { exportPages as exportOldPages } from './search-index-old/export'
 import { importPage as importNewPage } from './search-index-new/import'
 import * as newIndex from './search-index-new'
-import { global } from 'core-js/library/web/timers';
+import * as testData from './index.test.data'
 
 async function doIntegrationTest() {
     const visit1 = Date.now().toString()
     await search.addPage({
         pageDoc: {
-            _id: 'test-id-1',
+            _id: 'page/test-id-1',
             url: 'https://www.test.com/test',
             content: {
                 fullText: 'the wild fox jumped over the hairy red hen',
@@ -29,12 +30,11 @@ async function doIntegrationTest() {
         query: 'fox',
         mapResultsFunc: async results => results,
     })
-    console.log(results1[0])
     expect(results1).toEqual([
         expect.objectContaining({
-            id: 'test-id-1',
+            id: 'page/test-id-1',
             document: {
-                id: 'test-id-1',
+                id: 'page/test-id-1',
                 terms: new Set([
                     'term/wild',
                     'term/fox',
@@ -57,7 +57,7 @@ async function doIntegrationTest() {
     const visit2 = Date.now().toString()
     await search.addPage({
         pageDoc: {
-            _id: 'test-id-2',
+            _id: 'page/test-id-2',
             url: 'https://www.test.com/test2',
             content: {
                 fullText: 'the fox was wild',
@@ -73,9 +73,9 @@ async function doIntegrationTest() {
     })
     expect(results2).toEqual([
         expect.objectContaining({
-            id: 'test-id-2',
+            id: 'page/test-id-2',
             document: {
-                id: 'test-id-2',
+                id: 'page/test-id-2',
                 terms: new Set(['term/fox', 'term/wild']),
                 urlTerms: new Set([]),
                 titleTerms: new Set(['title/test', 'title/page']),
@@ -87,7 +87,7 @@ async function doIntegrationTest() {
             },
         }),
         expect.objectContaining({
-            id: 'test-id-1',
+            id: 'page/test-id-1',
             document: expect.objectContaining({
                 terms: new Set([
                     'term/wild',
@@ -101,14 +101,14 @@ async function doIntegrationTest() {
         }),
     ])
 
-    await search.delPages(['test-id-2'])
+    await search.delPages(['page/test-id-2'])
     const { docs: results3 } = await search.search({
         query: 'fox wild',
         mapResultsFunc: async results => results,
     })
     expect(results3).toEqual([
         expect.objectContaining({
-            id: 'test-id-1',
+            id: 'page/test-id-1',
             document: expect.objectContaining({
                 terms: new Set([
                     'term/wild',
@@ -133,43 +133,43 @@ describe('Old search index', () => {
     test('Exporting data', async () => {
         search.getBackend._reset({ useOld: true })
         oldIndex.init({ levelDown: memdown() })
+        await db.erase()
+        await db.put(testData.PAGE_DOC_1)
 
-        const visit1 = Date.now().toString()
+        const visit1 = Date.now()
+        const bookmark1 = (Date.now() + 5000)
         await search.addPage({
-            pageDoc: {
-                _id: 'test-id-1',
-                url: 'https://www.test.com/test',
-                content: {
-                    fullText: 'the wild fox jumped over the hairy red hen',
-                    title: 'test page',
-                },
-            },
+            pageDoc: testData.PAGE_DOC_1,
             bookmarkDocs: [],
             visits: [visit1],
         })
-        const visit2 = Date.now().toString()
-        await search.addPage({
-            pageDoc: {
-                _id: 'test-id-2',
-                url: 'https://www.test.com/test2',
-                content: {
-                    fullText: 'the fox was wild',
-                    title: 'test page 2',
-                },
-            },
-            bookmarkDocs: [],
-            visits: [visit2],
-        })
+        await search.addTag(testData.PAGE_DOC_1.url, 'virus')
+        await search.addTag(testData.PAGE_DOC_1.url, 'fix')
+        await search.addBookmark({ url: testData.PAGE_DOC_1.url, timestamp: bookmark1, tabId: 25 })
 
         const stream = exportOldPages()
+        const exported = []
         await new Promise((resolve, reject) => {
             stream
                 .on('data', (obj) => {
-                    console.log('Got data!', obj)
+                    exported.push(obj)
                 })
                 .on('error', reject)
                 .on('end', resolve)
         })
+        expect(exported).toEqual([{
+            url: "https://www.2-spyware.com/remove-skype-virus.html",
+            content: {
+                lang: testData.PAGE_DOC_1.content.lang,
+                title: testData.PAGE_DOC_1.content.title,
+                fullText: testData.PAGE_DOC_1.content.fullText,
+                keywords: testData.PAGE_DOC_1.content.keywords,
+                description: testData.PAGE_DOC_1.content.description
+            },
+            visits: [{ timestamp: visit1 }],
+            tags: ['virus', 'fix'],
+            bookmark: bookmark1
+        }])
     })
 })
 
